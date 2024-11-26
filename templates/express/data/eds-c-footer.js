445fcb07e7bbfb0b866a28e0734e6fa889c47c33
Matteo Gildone <matteo.gildone@springernature.com>
2024-11-26 16:41:42 +0000

    minimal footer

--- templates/express/data/eds-c-footer.js
+++ templates/express/data/eds-c-footer.js
@@ -74,38 +74,7 @@ const edsCFooterData = {
 		"alt": "Springer Nature",
 		"link": "https://www.springernature.com/"
 	},
-	"currentYear": 2024,
-	"productFooter": "productFooter",
-	"corporateFooter": "corporateFooter",
-	"productGroups": [
-		{
-			"title": "Discover content",
-			"items": [
-				{
-					"url": "https://www.nature.com/siteindex",
-					"text": "Journals A-Z",
-					"dataAttributes": [
-						{
-							"name": "track-context",
-							"value": "journals a-z"
-						},
-						{
-							"name": "track-content-type",
-							"value": "footer"
-						},
-						{
-							"name": "track-label",
-							"value": "footer"
-						},
-						{
-							"name": "test",
-							"value": "yes-test"
-						}
-					]
-				}
-			]
-		}
-	]
+	"currentYear": 2024
 };
 
 export {
